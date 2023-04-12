0f03db76bf286851186327ba2150274f8c203a39
metacoding <getinthere@naver.com>
2023-04-12 16:25:46 +0900

    버그수정

--- src/main/java/shop/mtcoding/servicebank/controller/TransactionController.java
+++ src/main/java/shop/mtcoding/servicebank/controller/TransactionController.java
@@ -2,9 +2,10 @@ package shop.mtcoding.servicebank.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+import shop.mtcoding.servicebank.service.TransactionService;
 
 @RequiredArgsConstructor
 @RestController
-public class TransactionService {
+public class TransactionController {
     private final TransactionService transactionService;
 }
